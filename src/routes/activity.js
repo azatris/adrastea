737dfdff3e86f535b8130cfe7f70b837ab9d4675
Silver Taza <azatris@gmail.com>
2023-01-24 15:26:27 -0800

    Refactor

--- src/routes/activity.js
+++ src/routes/activity.js
@@ -3,20 +3,21 @@ const router = express.Router();
 const boredApiService = require('../boredapiservice');
 
 router.get('/', function(req, res, next) {
-	const activityCall = async () => {
+	const getTransformedActivity = async () => {
 		const response = await boredApiService.getActivity();
 		const activityJson = response.data;
 		const accessibility = activityJson.accessibility;
 		const price = activityJson.price;
-		const priceText = price === 0 ? "Free" : price <= 0.5 ? "Low" : "High"; // Assuming price is a number between 0 and 1
-		const accessibilityText = accessibility <= 0.25 ? "High" : accessibility <= 0.75 ? "Medium" : "Low"; // Assuming accessibility is a number between 0 and 1
 
 		// Override old values with new ones
+		const priceText = price === 0 ? "Free" : price <= 0.5 ? "Low" : "High"; // Assuming price is a number between 0 and 1
+		const accessibilityText = accessibility <= 0.25 ? "High" : accessibility <= 0.75 ? "Medium" : "Low"; // Assuming accessibility is a number between 0 and 1
 		activityJson.accessibility = accessibilityText;
 		activityJson.price = priceText;
-		res.send(activityJson);
+
+		return activityJson;
 	}
-	activityCall();
+	getTransformedActivity().then(activity => { res.send(activity); });
 });
 
 module.exports = router;
