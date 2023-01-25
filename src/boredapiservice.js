2bccc22c1f69c7032641a54c642763110fbdb447
Silver Taza <azatris@gmail.com>
2023-01-25 10:25:19 -0800

    Adjust activity API usage to reflect current user's preferences

--- src/boredapiservice.js
+++ src/boredapiservice.js
@@ -2,8 +2,54 @@ const axios = require('axios');
 const BASE_URL = 'http://www.boredapi.com/api';
 
 module.exports = {
-	getActivity: () => axios({
-		method: 'GET',
-		url: `${BASE_URL}/activity`
-	})
+	getActivity: (accessibilityLevel, priceLevel) => {
+		if (!accessibilityLevel && !priceLevel) {
+			return axios({
+				method: 'GET',
+				url: `${BASE_URL}/activity`
+			});
+		}
+
+		let epsilon = 0.00000000000000001; // The API has inclusive ranges, so we need to add a small number to the min params
+		let countParams = 0;
+		let url = `${BASE_URL}/activity?`;
+		if (accessibilityLevel) {
+			let minaccessibility = 0;
+			let maxaccessibility = 1;
+			if (accessibilityLevel === 'High') {
+				minaccessibility = 0;
+				maxaccessibility = 0.25;
+			} else if (accessibilityLevel === 'Medium') {
+				minaccessibility = 0.25 + epsilon;
+				maxaccessibility = 0.75;
+			} else if (accessibilityLevel === 'Low') {
+				minaccessibility = 0.75 + epsilon;
+				maxaccessibility = 1;
+			}
+			url += `minaccessibility=${minaccessibility}&maxaccessibility=${maxaccessibility}`;
+			countParams++;
+		}
+		if (priceLevel) {
+			if (countParams > 0) {
+				url += `&`;
+			}
+			let minprice = 0;
+			let maxprice = 1;
+			if (priceLevel === 'Free') {
+				minprice = 0;
+				maxprice = 0;
+			} else if (priceLevel === 'Low') {
+				minprice = epsilon;
+				maxprice = 0.5;
+			} else if (priceLevel === 'High') {
+				minprice = 0.5 + epsilon;
+				maxprice = 1;
+			}
+			url += `minprice=${minprice}&maxprice=${maxprice}`;
+		}
+		return axios({
+			method: 'GET',
+			url: url
+		});
+	}
 }
--- src/routes/activity.js
+++ src/routes/activity.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const boredApiService = require('../boredapiservice');
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 router.get('/', function(req, res, next) {
 	const getTransformedActivity = async () => {
-		const { data: activityJson } = await boredApiService.getActivity();
+		// If a user is created, let's use the last one created to get their accessibility and price to query the API
+		// const { data: lastUser } = await prisma.user.findMany(({ orderBy: { id: 'desc' }, take: 1 }));
+		const lastUser = await prisma.user.findFirst({ orderBy: { id: 'desc' } });
+		await prisma.$disconnect();
+
+		let activityJson;
+		if (lastUser) {
+			const { accessibility: accessibilityLevel, price: priceLevel } = lastUser;
+			const activity = await boredApiService.getActivity(accessibilityLevel, priceLevel);
+			activityJson = activity.data;
+		} else {
+			const activity = await boredApiService.getActivity();
+			activityJson = activity.data;
+		}
 		const { accessibility, price } = activityJson;
 
 		// Override old values with new ones
-		const priceText = price === 0 ? "Free" : price <= 0.5 ? "Low" : "High"; // Assuming price is a number between 0 and 1
-		const accessibilityText = accessibility <= 0.25 ? "High" : accessibility <= 0.75 ? "Medium" : "Low"; // Assuming accessibility is a number between 0 and 1
-		activityJson.accessibility = accessibilityText;
-		activityJson.price = priceText;
+		const priceType = price === 0 ? "Free" : price <= 0.5 ? "Low" : "High"; // Assuming price is a number between 0 and 1
+		const accessibilityType = accessibility <= 0.25 ? "High" : accessibility <= 0.75 ? "Medium" : "Low"; // Assuming accessibility is a number between 0 and 1
+		activityJson.accessibility = accessibilityType;
+		activityJson.price = priceType;
 
 		return activityJson;
 	}
