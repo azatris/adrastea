a3bcd122ef30dd62ff1bd2c4aa4fca3ea937ccea
Silver Taza <azatris@gmail.com>
2023-01-27 11:44:10 -0800

    Add React client using create-react-app

---
+++ client/README.md
@@ -0,0 +1,7 @@
+# Client for the Activity Generator
+
+## Notes
+
+Could generate icons on build rather than having them included in the repo.
+
+
--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import error500Router from "./routes/error500Router";
 import { errorResponder, invalidPathHandler } from "./middleware";
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 5000;
 
 const logger = require("morgan");
 
