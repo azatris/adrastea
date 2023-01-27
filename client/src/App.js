9b0b5f9fe551b104b4211ad67bd933a5131bdcf8
Silver Taza <azatris@gmail.com>
2023-01-27 12:23:55 -0800

    Add JoyUI to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import './App.scss';
+import {Button, CssBaseline, CssVarsProvider, Divider, Grid, Stack, Typography} from "@mui/joy";
+import DarkToggle from "./DarkToggle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <CssVarsProvider>
+          <CssBaseline />
+          <div className="App">
+              <Grid container spacing={2} justifyContent="flex-start" sx={{ flexGrow: 1, py: 2, px: 3 }}>
+                  <Grid xs={6}>
+                      <Stack direction="row" justifyContent="flex-start">
+                          <Typography level="h2" component="h1">
+                              🐱‍🐉 Generate Activity App
+                          </Typography>
+                      </Stack>
+                  </Grid>
+                  <Grid xs={6}>
+                      <Stack direction="row" justifyContent="flex-end" spacing={2}>
+                          <Button variant="outlined">😀 Create Profile</Button>
+                          <DarkToggle />
+                      </Stack>
+                  </Grid>
+              </Grid>
+              <Divider />
+
+          </div>
+      </CssVarsProvider>
   );
 }
 
---
+++ client/src/DarkToggle.js
@@ -0,0 +1,17 @@
+import { useColorScheme } from '@mui/joy/styles';
+import Button from '@mui/joy/Button';
+
+function DarkToggle() {
+	const { mode, setMode } = useColorScheme();
+	return (
+		<Button
+			variant="outlined"
+			color="neutral"
+			onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+		>
+			{mode === 'dark' ? '🌞' : '🌙'}
+		</Button>
+	);
+}
+
+export default DarkToggle;
